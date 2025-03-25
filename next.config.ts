dbb9d251385ba1028c40665ea2c8370cc95c2c8c
bdmtnz <martinez_o@hotmail.es>
2025-03-24 21:38:06 -0500

    feat: refresh aviationstack token

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     PUBLIC_API_URL: "https://api.aviationstack.com/v1",
-    PUBLIC_API_KEY: "5c8f0e80ba66dbff26982aacdde4c386",
+    PUBLIC_API_KEY: "c65da17a1a923daa75234c31040b525a",
     PUBLIC_MAPS_API_KEY: "AIzaSyBjD3wO_UKFT-PJ8IHJjix9EK1QCyVmxZc"
   },
   experimental: {
